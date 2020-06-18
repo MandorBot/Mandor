9a535a2f0cb01cd00ec9547e8f92861a9bb43db5
fbunaren <fransiscusbunaren@gmail.com>
2020-06-18 16:14:44 +0700

    Implement on message event

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import os
 
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!vote':
+        response = ":clipboard: Vote started, please add project names by using the command -vote!add project_name"
+        await message.channel.send(response)
 
 client.run(TOKEN)
