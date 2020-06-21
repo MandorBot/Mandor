d464d19a00788d9ae55d01698c2fc44a2ccfdd79
fbunaren <fransiscusbunaren@gmail.com>
2020-06-21 22:50:51 +0700

    Implement vote, prevent double voters. Add checking to add and project name

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
-projects = []
+projects = {}
 
 @client.event
 async def on_ready():
@@ -25,11 +25,32 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global projects
     if message.author == client.user:
         return
-
+    
     if "!pm" in message.content:
         response = "Unrecognized command, please use `!pm help` for a full command list"
+        if "clear" in message.content:
+            response = """```Projects has been cleared!```"""
+            projects = {}
+
+        if "vote" in message.content:
+            token = message.content.lower().split()
+            if token[-1] == "vote" or token[-2] == "vote":
+                response = """```Please specify the project that you want to vote: !pm vote [projectName] [ratio]```"""
+            else:
+                try:
+                    if message.author in projects[token[-2]]['voters']:
+                        response = """@{}, you cannot vote multiple times!  """.format(message.author.split("#")[0])
+                    else:
+                        ratio = token[-1].split("/")
+                        ratio = int(ratio[0]) / int(ratio[1])
+                        projects[token[-2]]['vote'] += ratio
+                        projects[token[-2]]['voters'].add(message.author) 
+                        response = """@{}, thanks for voting!""".format(message.author.split("#")[0])
+                except:
+                    pass
 
         if "help" in message.content:
             response = """```Here are available commands:
@@ -44,10 +65,13 @@ async def on_message(message):
 
             if "add" in message.content:
                 response = "Please specify a name for the project"
-                projectName = message.content.replace("!pm project add", "").strip()
+                projectName = message.content.replace("!pm project add", "").strip().replace(" ","_").lower()
                 if projectName:
-                    response = "{} has been added".format(projectName)
-                    projects.append(projectName)
+                    if (projectName not in projects):
+                        response = "{} has been added".format(projectName)
+                        projects[projectName] = {'vote':0,'voters':set()}
+                    else:
+                        response = "{} is already exist!".format(projectName)
 
             if "remove" in message.content:
                 response = "Please specify a name for the project"
@@ -63,9 +87,9 @@ async def on_message(message):
             if "list" in message.content:
                 response = "There are no saved projects for now :("
                 if projects:
-                    response = "```List of saved projects:\n"
-                    for project in projects:
-                        response = response + project + "\n"
+                    response = "```📋 List of saved projects:\n"
+                    for index,project in enumerate(projects):
+                        response = str(response) + str(1+index) + ". " + str(project) + " (" + str(projects[project]['vote']) + " votes) " + "\n"
                     response = response + "```"              
 
         await message.channel.send(response)
